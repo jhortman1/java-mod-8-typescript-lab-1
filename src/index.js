3695bef2577057c51853d186477d97278a9bf34c
James Hortman <jamesdhortman@gmail.com>
2022-08-15 12:00:38 -0400

    complete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function prepend(numSpaces, inputString) {
-    // your code here
-    var newString = "";
-    for (var i = 0; i < numSpaces; i++) {
-        newString += " ";
-    }
-    return newString.concat(inputString);
-}
-console.log(prepend(5, "Hello"));
